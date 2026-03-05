fa6c50004d847bf7f61cfc6623205fb6a9271812
Kamil Baldyga <baldygakamil@gmail.com>
2026-03-05 19:33:12 +0100

    add template colors to lua/candide/init.lua

--- lua/candide/init.lua
+++ lua/candide/init.lua
@@ -6,15 +6,15 @@ vim.g.colors_name = "candide"
 
 function M.setup()
 	local colors = {
-		bg	= "",
-		fg	= "",
-		orange	= "",
-		red		= "",
-		green	= "",
-		light_green = "",
-		blue	= "",
-		purple	= "",
-		gray	= "",
+		bg	= "#000000",
+		fg	= "#C9C9D9",
+		orange	= "#FFD97C",
+		red		= "#FF7B72",
+		green	= "#D1FFE1",
+		light_green = "#E1FFF8",
+		blue	= "#51FEFF",
+		purple	= "#FFB8FF",
+		gray	= "#FCFFF7",
 	}
 	local set = vim.api.nvim_set_hl
 
@@ -22,7 +22,7 @@ function M.setup()
 	set(0, "Comment", { fg = colors.light_green, italic = true })
 	set(0, "String", { fg = colors.green })
 	set(0, "Function", { fg = colors.blue })
-	set(0, "Keyword", { fg = colors.purple })
+	set(0, "Keyword", { fg = colors.orange })
 	set(0, "Error", { fg = colors.red, bold = true })
 end
 
