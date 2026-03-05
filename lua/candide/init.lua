d43fc73f5bab83073946b3bef8630e2c304710d4
Kamil Baldyga <baldygakamil@gmail.com>
2026-03-05 19:16:29 +0100

    add template for lua/candide/init.lua

---
+++ lua/candide/init.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.setup()
+	local colors = {
+		bg	= "",
+		fg	= "",
+		orange	= "",
+		red		= "",
+		green	= "",
+		light_green = "",
+		blue	= "",
+		purple	= "",
+		gray	= "",
+	}
+	local set = vim.api.nvim_set_hl
+
+	set(0, "Normal", { fg = colors.fg, bg = colors.bg })
+	set(0, "Comment", { fg = colors.light_green, italic = true })
+	set(0, "String", { fg = colors.green })
+	set(0, "Function", { fg = colors.blue })
+	set(0, "Keyword", { fg = colors.purple })
+	set(0, "Error", { fg = colors.red, bold = true })
+end
+
+return M
